0dfc5f6290bfe39b155a697046be7a50c1e236ce
yurimutz <yurimutz02@gmail.com>
2024-11-20 18:01:25 -0300

    tad01 com tabuleiro e jogada

--- 04_TAD_simples/TAD_01/Respostas/YuriMutz/jogada.c
+++ 04_TAD_simples/TAD_01/Respostas/YuriMutz/jogada.c
@@ -1,10 +1,32 @@
 #include "jogada.h"
 #include <stdio.h>
 
-tJogada LeJogada();
+tJogada LeJogada(){
+    tJogada jogada;
+    jogada.x = 1001, jogada.y = 1001; 
+    printf("Digite uma posicao (x e y):\n");
+    scanf("%d%d", &jogada.x, &jogada.y);
 
-int ObtemJogadaX(tJogada jogada);
+    if(jogada.x != 1001 && jogada.y != 1001){
+        jogada.sucesso = 1;
+    } else {
+        jogada.sucesso = 0;
+    }
+    return jogada;
+}
 
-int ObtemJogadaY(tJogada jogada);
+int ObtemJogadaX(tJogada jogada){
+    return jogada.x;
+}
 
-int FoiJogadaBemSucedida(tJogada jogada);
+int ObtemJogadaY(tJogada jogada){
+    return jogada.y;
+}
+
+int FoiJogadaBemSucedida(tJogada jogada){
+    if(jogada.sucesso == 1){
+        return 1;
+    } else {
+        return 0;
+    }
+}
--- 04_TAD_simples/TAD_01/Respostas/YuriMutz/jogador.c
+++ 04_TAD_simples/TAD_01/Respostas/YuriMutz/jogador.c
@@ -1,7 +1,11 @@
 #include "jogador.h"
 #include <stdio.h>
 
-tJogador CriaJogador(int idJogador);
+tJogador CriaJogador(int idJogador){
+    tJogador jogador;
+    jogador.id = idJogador;
+    return jogador;
+}
 
 tTabuleiro JogaJogador(tJogador jogador, tTabuleiro tabuleiro);
 
