384df1ce2296bf5217a670afa3742389d01b2699
yurimutz <yurimutz02@gmail.com>
2024-11-21 14:53:29 -0300

    tad01 jogo e jogador atualizdos

--- 04_TAD_simples/TAD_01/Respostas/YuriMutz/jogador.c
+++ 04_TAD_simples/TAD_01/Respostas/YuriMutz/jogador.c
@@ -1,5 +1,6 @@
 #include "jogador.h"
 #include <stdio.h>
+#include "jogada.h"
 
 tJogador CriaJogador(int idJogador){
     tJogador jogador;
@@ -7,6 +8,11 @@ tJogador CriaJogador(int idJogador){
     return jogador;
 }
 
-tTabuleiro JogaJogador(tJogador jogador, tTabuleiro tabuleiro);
+tTabuleiro JogaJogador(tJogador jogador, tTabuleiro tabuleiro){
+    tJogada jogada;
+    jogada = LeJogada();
+    MarcaPosicaoTabuleiro(tabuleiro, jogador.id, jogada.x, jogada.y);
+    return tabuleiro;
+}
 
 int VenceuJogador(tJogador jogador, tTabuleiro tabuleiro);
--- 04_TAD_simples/TAD_01/Respostas/YuriMutz/jogo.c
+++ 04_TAD_simples/TAD_01/Respostas/YuriMutz/jogo.c
@@ -1,10 +1,57 @@
 #include "jogo.h"
+#include "jogador.h"
+#include "tabuleiro.h"
+#include "jogada.h"
 #include <stdio.h>
 
-tJogo CriaJogo();
+// typedef struct{
+//     tTabuleiro tabuleiro;
+//     tJogador jogador1;
+//     tJogador jogador2;
+    
+// } tJogo;
 
-void ComecaJogo(tJogo jogo);
+tJogo CriaJogo(){
+    tJogo jogo;
+    jogo.tabuleiro = CriaTabuleiro();
+    jogo.jogador1 = CriaJogador(ID_JOGADOR_1);
+    jogo.jogador2 = CriaJogador(ID_JOGADOR_2);
+    return jogo;
+}
 
-int AcabouJogo(tJogo jogo);
+void ComecaJogo(tJogo jogo){
+    tJogador teste = jogo.jogador1;
+   
+    while(1){
+        while(!AcabouJogo(jogo)){
+            jogo.tabuleiro = JogaJogador(teste, jogo.tabuleiro);
+            ImprimeTabuleiro(jogo.tabuleiro);
+            if(teste.id == ID_JOGADOR_1){
+                teste = jogo.jogador2;
+            } else {
+                teste = jogo.jogador1;
+            }
+        }
+        if(ContinuaJogo()){
+            jogo = CriaJogo();
+            continue;
+        } else {
+            break;
+        }
+    }
+        
+}
 
-int ContinuaJogo();
+int AcabouJogo(tJogo jogo){
+    if(TemPosicaoLivreTabuleiro(jogo.tabuleiro)){
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+int ContinuaJogo(){
+    char modo[3];
+    int i = 0;
+    
+}
