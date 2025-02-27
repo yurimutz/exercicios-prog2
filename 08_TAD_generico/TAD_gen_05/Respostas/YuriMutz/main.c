3b6db635473450803cca7a06f34df74028d9242c
yurimutz <yurimutz02@gmail.com>
2025-02-27 09:39:34 -0300

    tad generico 5 (refiz para treinar para EA)

--- 08_TAD_generico/TAD_gen_05/Respostas/YuriMutz/main.c
+++ 08_TAD_generico/TAD_gen_05/Respostas/YuriMutz/main.c
@@ -7,33 +7,34 @@
 
 int main(){
 
-    int num = 0, pri = 0;
-    char mode;
+    int numElem = 0;
 
     printf("Digite o numero de tarefas:");
 
-    scanf("%d\n", &num);
+    scanf("%d\n", &numElem);
 
-    tAgendaTarefas *agenda = CriaAgendaDeTarefas(num);
+    tAgendaTarefas *tar = CriaAgendaDeTarefas(numElem);
 
-    for(int i = 0; i < num; i++){
+    for(int i = 0; i < numElem; i++){
 
-        scanf("%d", &pri);
+        int prioridade = 0;
+        char mode;
+
+        scanf("%d", &prioridade);
         scanf(" %c ", &mode);
-        //scanf("%*[\n]");
+        scanf("%*[\n]");
 
         switch (mode)
         {
         case 'I':
-
-            char msg[50];
+            
+            char nome[25];
+            scanf("%[^\n]", nome);
             scanf("%*[\n]");
-            scanf("%[^\n]", msg);
-            scanf("%*[\n]");
 
-            tImpr *imp = CriaTarefaImprimir(msg);
+            tImpr *impr = CriaTarefaImprimir(nome);
 
-            CadastraTarefaNaAgenda(agenda, pri, imp, ExecutaTarefaImprimir, DestroiTarefaImprimir);
+            CadastraTarefaNaAgenda(tar, prioridade, impr, ExecutaTarefaImprimir, DestroiTarefaImprimir);
 
             break;
 
@@ -44,7 +45,7 @@ int main(){
 
             tMult *mult = CriaTarefaMultiplicar(n1, n2);
 
-            CadastraTarefaNaAgenda(agenda, pri, mult, ExecutaTarefaMultiplicar, DestroiTarefaMultiplicar);
+            CadastraTarefaNaAgenda(tar, prioridade, mult, ExecutaTarefaMultiplicar, DestroiTarefaMultiplicar);
 
             break;
 
@@ -55,7 +56,7 @@ int main(){
 
             tSoma *soma = CriaTarefaSoma(s1, s2);
 
-            CadastraTarefaNaAgenda(agenda, pri, soma, ExecutaTarefaSoma, DestroiTarefaSoma);
+            CadastraTarefaNaAgenda(tar, prioridade, soma, ExecutaTarefaSoma, DestroiTarefaSoma);
 
             break;
         
@@ -63,21 +64,103 @@ int main(){
 
             printf("\n");
             printf("Digite um tipo de tarefa suportado (I/S/M))");
-            scanf("%[^\n]", msg);
+            scanf("%[^\n]", nome);
             scanf("%*[\n]");
             i--;
-            
+
             break;
         }
 
-
-
     }
 
-    ExecutarTarefasDaAgenda(agenda);
+    ExecutarTarefasDaAgenda(tar);
 
-    DestroiAgendaDeTarefas(agenda);
-
+    DestroiAgendaDeTarefas(tar);
+                            
     return 0;
 
 }
+
+// #include "agendatarefas.h"
+// #include "tarefasoma.h"
+// #include "tarefamultiplica.h"
+// #include "tarefaimprime.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int main(){
+
+//     int num = 0, pri = 0;
+//     char mode;
+
+//     printf("Digite o numero de tarefas:");
+
+//     scanf("%d\n", &num);
+
+//     tAgendaTarefas *agenda = CriaAgendaDeTarefas(num);
+
+//     for(int i = 0; i < num; i++){
+
+//         scanf("%d", &pri);
+//         scanf(" %c ", &mode);
+//         //scanf("%*[\n]");
+
+//         switch (mode)
+//         {
+//         case 'I':
+
+//             char msg[50];
+//             scanf("%*[\n]");
+//             scanf("%[^\n]", msg);
+//             scanf("%*[\n]");
+
+//             tImpr *imp = CriaTarefaImprimir(msg);
+
+//             CadastraTarefaNaAgenda(agenda, pri, imp, ExecutaTarefaImprimir, DestroiTarefaImprimir);
+
+//             break;
+
+//         case 'M':
+            
+//             float n1 = 0, n2 = 0;
+//             scanf("%f %f\n", &n1, &n2);
+
+//             tMult *mult = CriaTarefaMultiplicar(n1, n2);
+
+//             CadastraTarefaNaAgenda(agenda, pri, mult, ExecutaTarefaMultiplicar, DestroiTarefaMultiplicar);
+
+//             break;
+
+//         case 'S':
+            
+//             float s1 = 0, s2 = 0;
+//             scanf("%f %f\n", &s1, &s2);
+
+//             tSoma *soma = CriaTarefaSoma(s1, s2);
+
+//             CadastraTarefaNaAgenda(agenda, pri, soma, ExecutaTarefaSoma, DestroiTarefaSoma);
+
+//             break;
+        
+//         default:
+
+//             printf("\n");
+//             printf("Digite um tipo de tarefa suportado (I/S/M))");
+//             scanf("%[^\n]", msg);
+//             scanf("%*[\n]");
+//             i--;
+            
+//             break;
+//         }
+
+
+
+//     }
+
+//     ExecutarTarefasDaAgenda(agenda);
+
+//     DestroiAgendaDeTarefas(agenda);
+
+//     return 0;
+
+// }
